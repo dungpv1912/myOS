09237833dd86cfc5690d8cfa7f8bf0b3560ba415
PhanVietDung <phanvietdung1998@gmail.com>
2024-04-20 23:46:14 -0700

    add Booting, Memory Segments, Global Descriptor Table

---
+++ kernel.cpp
@@ -0,0 +1,27 @@
+#include "gdt.h"
+#include "type.h"
+
+void printf(char* str){
+    uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    for(int i = 0;str[i]!='\0';i++){
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors(){
+    for (constructor* i = &start_ctors; i != &end_ctors;i++){
+        (*i)();
+    }
+}
+
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber){
+    printf("Hello world! --- dungpv12");
+
+    GlobalDescriptorTable gdt;
+
+    while(1);
+}
